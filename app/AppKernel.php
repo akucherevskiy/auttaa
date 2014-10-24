dc3c19c5667f37065223af97bfc5997d1f7ecc17
akucherevskiy <a.kucherevskiy@gmail.com>
2014-10-24 22:49:21 +0300

    add landing, bootstrap, WebBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,11 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            $bundles[] = new Auttaa\WebBundle\AuttaaWebBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new FOS\UserBundle\FOSUserBundle();
         }
 
         return $bundles;
---
+++ src/Auttaa/WebBundle/AuttaaWebBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Auttaa\WebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AuttaaWebBundle extends Bundle
+{
+}
